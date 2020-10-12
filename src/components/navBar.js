26c7d5fb38eadaa16bda4d9e27c47ba846f78e46
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-12 13:17:42 -0600

    add logo to nav bar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function navbar() {
     return (
         <div className = "nav-bar">
+            <img className = "logo" src = "https://muzoonfire.com/wp-content/uploads/2018/01/Re-Size_Logo.png" alt = "muzo on fire logo"/>
             <p>About</p>
         </div>
     )
