db914d81fe0023a4c7c19105a86e327f2a3eaee6
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-12 14:17:22 -0600

    add and style 'home' to nav bar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,7 +5,14 @@ export default function navbar() {
     return (
         <div className = "nav-bar">
             <img className = "logo" src = "https://muzoonfire.com/wp-content/uploads/2018/01/Re-Size_Logo.png" alt = "muzo on fire logo"/>
-            <Link to = "/about" className = "about-link">About</Link>
+            <ul>
+                <li>
+                    <Link to = "/" className = "home-link">Home</Link>
+                </li>
+                <li>
+                    <Link to = "/about" className = "about-link">About</Link>
+                </li>
+            </ul>
         </div>
     )
 }
