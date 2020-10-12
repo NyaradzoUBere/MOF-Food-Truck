c25043b996a395391114a156765db2690de19b93
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-12 14:01:58 -0600

    provide summary

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            <h1>About Us</h1>
-        </div>
+        <>
+            <div className = "our-journey">
+                <h1 className = "journey-header">Our Journey</h1>
+            </div>
+            <div className = "paragraph">
+                <p className = "journey-p">
+                Muzo on Fire is a heart-fueled, African infused food creation started by Muzo Bere in 2016. Originally from Zimbabwe, Southern Africa, Muzo has always associated food with family and community.
+                Growing up, the traditional kitchen was a place where family members often shared stories and jokes while the food was “on fire” – cooking fresh vegetables and whole grains from the fields on an open fire in the thatched roof hut, with heat and smoke wafting up to dry the best meat you have ever tasted.
+                Food was a way to welcome neighbors and show love through generosity. Living in the U.S. among friends from many different cultures, Muzo realized that the sharing of incredible food cultivated the same type of connections among people.
+                The food at Muzo on Fire is inspired by favorites from the African continent, mixed up with the complimentary flavors that lend a twist to tradition. Our menu is small with a rotating assortment that allows us to cook food from scratch with the freshest ingredients.
+                </p>
+            </div>
+        </>
     )
 }
